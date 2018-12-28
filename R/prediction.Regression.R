a1245d0515cb3b13e6d3e703bbe89a74e41c2aa4
cwilso6 <42617537+cwilso6@users.noreply.github.com>
2018-12-28 11:23:26 -0500

    Update prediction.Regression.R

--- R/prediction.Regression.R
+++ R/prediction.Regression.R
@@ -10,10 +10,10 @@
 #' @return predicted Sign of yhat, which is the final predicted outcome 
 #' @export
 
-prediction.Regression=function(model,ktest,train.outcome){
+prediction.Regression=function(model,ktest,outcome){
   product=list()
   product=lapply(1:length(ktest), function(a) ktest[[a]]*model$gamma[a])
   fushion=Reduce('+',product)
-  yhat=(model$alpha-model$alphastar)%*%t(kk)-model$b
+  yhat=(model$alpha-model$alpha.star)%*%t(fushion)-model$b
   return(yhat)
 }
